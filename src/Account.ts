d0e0d0eae1326cd49e7e4d8650296ab67dd85ada
hielo <alejandro@espaciomexico.net>
2015-11-26 17:59:44 -0600

    Adding headers description comments

--- src/Account.ts
+++ src/Account.ts
@@ -1,5 +1,29 @@
 import Set = collections.Set;
 
+/**
+ ***************************************************************************************************
+ * Represents a login account in the application, could also have been called a User or a Principal,
+ * see more below.
+ * <p>
+ * Note that what we call an Account here is what many applications call a 'User'.  We chose to use
+ * the term 'Account' because we believe that the semantics of the term 'User' are closer to those
+ * of a Person or Organization, which are represented by the biz.janux.people.Party interface and
+ * its sub-interfaces and implementing classes.  If necessary, the Person or Organization that
+ * 'owns' the Account can be referenced by the getParty field.  Thus, the user can be considered to
+ * be the Party for whom the Account was created, although in many cases such a Party may not be
+ * a Person or may not even exist (such as is the case for a 'system' account).
+ * </p><p>
+ * In an Authentication and Authorization context, another term for an Account is a Principal
+ * representing an identity (the account name) and a set of credentials auntenticating that
+ * identity (a password, or digital certificate).  We chose the term Account because we feel it is
+ * most accessible to every day business people than the more technical term Principal.
+ * </p>
+ *
+ * @author  <a href="mailto:philippe.paravicini@janux.org">Philippe Paravicini</a>
+ * @since 0.1
+ ***************************************************************************************************
+ */
+
 interface Account extends AuthorizationHolder{
 	/** A unique name that identifies this Account - returns the same value as the getUsername inherited frmo the UserDetails interface */
 	getName(): string
