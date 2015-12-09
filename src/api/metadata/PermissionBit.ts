2e5c2cea03c3b48c5fc4cb1dea6d7eac8073744c
hielo <alejandro@espaciomexico.net>
2015-12-08 19:14:15 -0600

    Rename PermissionBit interface and implementation

--- src/api/metadata/PermissionBit.ts
+++ src/api/metadata/PermissionBit.ts
@@ -1,12 +1,13 @@
+import {iAuthorizationContext} from "./AuthorizationContext";
 /**
  ***************************************************************************************************
- * Class representing an individual PermissionBit within a specific AuthorizationContext; a PermissionBit is
- * only meaningful in the context of the AuthorizationContext that defines it: for example, a AuthorizationContext
+ * Class representing an individual iPermissionBit within a specific iAuthorizationContext; a iPermissionBit is
+ * only meaningful in the context of the iAuthorizationContext that defines it: for example, a iAuthorizationContext
  * named 'PERSON' may define Permissions with names 'CREATE', 'READ', 'UPDATE', 'DISABLE',
  * 'PURGE', that define the kind of operations on Persons that may be restricted by the security
- * system; see the javadoc of AuthorizationContext for a more detailed discussion.
+ * system; see the javadoc of iAuthorizationContext for a more detailed discussion.
  * <p>
- * The PermissionBit interface provides for defining the bit position of the PermissionBit within a bit
+ * The iPermissionBit interface provides for defining the bit position of the iPermissionBit within a bit
  * mask (0, 1, 2, 3, etc...), and a convenience method for returning the long value of that bit
  * position (that is 2 taken to the power of the bitPosition, e.g. 1, 2, 4, 8...)
  * </p>
@@ -16,41 +17,40 @@
  ***************************************************************************************************
  */
 
-interface PermissionBit
-{
-	/**	
-	 * Short-hand name for this PermissionBit (e.g.: READ), 
-	 * unique in the context of the containing AuthorizationContext 
+export interface iPermissionBit {
+	/**
+	 * Short-hand name for this iPermissionBit (e.g.: READ),
+	 * unique in the context of the containing iAuthorizationContext
 	 */
 	getName(): string
-	setName(name: string): void
+	setName(name:string): void
 
-	/** 
-	 * The position of the PermissionBit within the bit mask defined by the AuthorizationContext, should be a
-	 * sequential integer relative to the sequence defined by the AuthorizationContext; so if a AuthorizationContext
+	/**
+	 * The position of the iPermissionBit within the bit mask defined by the iAuthorizationContext, should be a
+	 * sequential integer relative to the sequence defined by the iAuthorizationContext; so if a iAuthorizationContext
 	 * defines 5 permissions, this should be a number between 0 and 4 that is not used by any of the
-	 * other Permissions in the AuthorizationContext
+	 * other Permissions in the iAuthorizationContext
 	 */
 	getPosition(): number
-	setPosition(pos: number): void
+	setPosition(pos:number): void
 
 	/** A convenience method that returns 2 to the power of the bitPosition */
 	getValue(): number
 
-	/** The AuthorizationContext that contains/uses this PermissionBit */
-	getAuthorizationContext(): AuthorizationContext
-	setAuthorizationContext(authContext: AuthorizationContext): void
+	/** The iAuthorizationContext that contains/uses this iPermissionBit */
+	getAuthorizationContext(): iAuthorizationContext
+	setAuthorizationContext(authContext:iAuthorizationContext): void
 
-	/**	 Human readable description of this PermissionBit */
+	/**     Human readable description of this iPermissionBit */
 	getDescription(): string
-	setDescription(description: string): void
+	setDescription(description:string): void
 
-	/** 
+	/**
 	 * used to display the sort order independently from the Bit's Position, defaults to the
-	 * getPosition if not set explicitly 
+	 * getPosition if not set explicitly
 	 */
 	getSortOrder(): number
-	setSortOrder(i: number): void
+	setSortOrder(i:number): void
 }
 
 
--- src/impl/PermissionBit.ts
+++ src/impl/PermissionBit.ts
@@ -1,64 +1,67 @@
-class PermissionBitImpl implements PermissionBit
-{
-    private name: string;
-    private position: number = -1;
+import {iPermissionBit} from "../api/metadata/PermissionBit";
+import {iAuthorizationContext} from "../api/metadata/AuthorizationContext";
 
-    private description: string;
-    private authContext: AuthorizationContext;
-    private sortOrder: number;
+export default class PermissionBit implements iPermissionBit {
+    private name:string;
+    private position:number = -1;
 
-    constructor(name: string, description: string) {
+    private description:string;
+    private authContext:iAuthorizationContext;
+    private sortOrder:number;
+
+    constructor(name:string, description:string, sortOrder?:number) {
 
         this.setName(name);
         this.setDescription(description);
+        this.sortOrder = sortOrder;
     }
 
-    getName(): string {
+    getName():string {
         return this.name;
     }
 
-    setName(name: string): void {
+    setName(name:string):void {
         this.name = name;
     }
 
-    getPosition(): number {
+    getPosition():number {
         return this.position;
     }
 
-    setPosition(position: number): void {
+    setPosition(position:number):void {
         this.position = position;
     }
 
-    getValue(): number {
+    getValue():number {
         return Math.pow(2.0, this.getPosition());
     }
 
-    getAuthorizationContext(): AuthorizationContext {
+    getAuthorizationContext():iAuthorizationContext {
         return this.authContext;
     }
 
-    setAuthorizationContext(bitmask: AuthorizationContext): void {
+    setAuthorizationContext(bitmask:iAuthorizationContext):void {
         this.authContext = bitmask;
     }
 
-    getDescription(): string {
+    getDescription():string {
         return this.description;
     }
 
-    setDescription(description: string): void {
+    setDescription(description:string):void {
         this.description = description;
     }
 
-    getSortOrder(): number
-    {
+    getSortOrder():number {
         if (this.sortOrder == null)
             this.sortOrder = this.getPosition();
 
         return this.sortOrder;
     }
 
-    setSortOrder(i: number): void {
+    setSortOrder(i:number):void {
         this.sortOrder = i;
     }
 
 } // end class PermissionBitImpl
+
