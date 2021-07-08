a1a186f8a529b4a3d0639b98ed597c0660a4e184
José Luis Palillero Huerta <joseluispalillero@gmail.com>
2021-07-08 15:21:24 -0500

    Task: #177751808 update build node 12

--- gulp/clean.tasks.js
+++ gulp/clean.tasks.js
@@ -1,31 +1,33 @@
 'use strict';
 //
-// Clean build and doc 
+// Clean build and doc
 //
 
 var	del = require('del'),
 	path     = require('path');
 
 module.exports = function(gulp) {
-	
+
 	var cfg = gulp.cfg;
 
 	//
 	// Remove all generated JavaScript files from TypeScript compilation process.
 	//
-	gulp.task('clean:build', function (cb) {
+	gulp.task('clean:build', (cb) => {
 		console.log('cleaning '+cfg.dir.dist+' directory...');
 		// delete the files
-		del([path.join(cfg.dir.dist,'**','*')], cb);
+		del([path.join(cfg.dir.dist,'**','*')]);
+		cb();
 	});
-	
+
 	//
 	// Remove all generated TypeScript docs.
 	//
-	gulp.task('clean:doc', function (cb) {
+	gulp.task('clean:doc', (cb) => {
 		console.log('cleaning ts documentation directory...');
 		// delete the files
-		del([path.join(cfg.dir.doc,'*')], cb);
+		del([path.join(cfg.dir.doc,'*')]);
+		cb();
 	});
 };
 
